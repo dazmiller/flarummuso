db5855fb5667a69a7206e0254c4fabec91cf01e8
Darren Miller <dazmiller@gmail.com>
2018-11-15 06:57:42 +1100

    first add

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost/flarum',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
